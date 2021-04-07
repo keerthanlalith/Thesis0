c6d91c9d98bf63c4df1602c0ccf0e832eee9b9d7
keerthanlalith <keerthanlalith@gmail.com>
2021-04-07 18:18:57 +0200

    Updated datagen

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # Thesis0
 
 ## Requirements 
-This implementation has been tested with Python 3.5 on OS X High Sierra and Ubuntu 14.04. 
+This implementation has been tested with Python 3.5 on Ubuntu 18.04. 
 
 ```Shell
 # 1) Clone repository 
 git clone https://https://github.com/keerthanlalith/Thesis0
-cd ILPO
+cd Thesis0
 
 # 2) Install requirements
 pip install -r requirements.txt
 ``` 
+
+## Collecting expert data 
+[OpenAI Baselines](https://github.com/openai/baselines) to obtain these trajectories. 
+Collecting the data consists of two steps: 
+1) training the expert and 
+2) running the learned policy and saving the observed state trajectories to disk.
+
+```Shell
+# 1) Train expert
+python train_expert/train_cartpole.py
+
+# 2) Collect state trajectories 
+python enjoy_cartpole_custom.py
+```
+
+Once done running, the expert policy from step 1 is written to [Data/cartpole.pkl](https://github.com/keerthanlalith/Thesis0/tree/main/Data/). Then, step 2 loads and runs the policy and saves the observed states, actions, next states  and difference(between the current state and the nextstate) to [Data](https://github.com/keerthanlalith/Thesis0/tree/main/Data). 
+
--- enjoy_cartpole_custom.py
+++ enjoy_cartpole_custom.py
@@ -3,185 +3,184 @@ import numpy as np
 from baselines import deepq
 import pickle
 
-train = 0
-test = 1
+train = 1
+test = 0
 MODE = 0
 
 BEST = 1
 DEFAULT = 0.75
-RANDOM = DEFAULT
-num_steps = 50000
-
-if MODE == test:
-    RANDOM = BEST
-    num_steps = 1000
 
 MODEL = "Data/cartpole_model.pkl"
 ENV = "CartPole-v1"
 
-FILE = "Data/cartpole.txt"
-BC_FILE = "Data/cartpole_bc.txt"
-D_FILE = "Data/cartpole_d.txt"
-
-if MODE == test:
-    FILE = "Data/Tcartpole.txt"
-    BC_FILE = "Data/Tcartpole_bc.txt"
-    D_FILE = "Data/Tcartpole_d.txt"
-
-
-
-
 
 def main():
-    s=[]
-    a=[]
-    ns=[]
-    d=[]
     env = gym.make(ENV)
     act = deepq.learn(env, network='mlp', total_timesteps=0, load_path=MODEL)
-    steps = 0
-    outfile = open(FILE, 'w')
-    bcfile = open(BC_FILE, 'w')
-    dfile = open(D_FILE, 'w')
     
-    total_reward = 0
-    episodes = 0
-
-    while steps < num_steps:
-        obs, done = env.reset(), False
-        episode_rew = 0
-        while not done:
-            #env.render()
-            #if steps % 200==0:
-            #    obs, done = env.reset(), False
-            state_1 = obs
-
-            if np.random.uniform(0,1) <= RANDOM:
-                action = act(obs[None])[0]
-                obs, rew, done, _ = env.step(action)
-            else:
-                action = env.action_space.sample()
-                obs, rew, done, _ = env.step(action)
-                continue
-            
-            s.append(state_1)
-            a.append(action)
-            state_2 = obs
-            diff = state_2 - state_1
-
-            ns.append(state_2)
-            d.append(diff)
-
-            # write to AON file
-            to_write = '['
-            for w in state_1:
-                to_write += str(w) + ','
-            to_write = to_write[:-1]
-            to_write += ']'
-
-            outfile.write(to_write)
-            outfile.write(" ")
-            to_write = '['
-
-            for w in state_2:
-                to_write += str(w) + ','
-            to_write = to_write[:-1]
-            to_write += ']'
-
-            outfile.write(to_write)
-            outfile.write("\n")
-
-            # write to BC file
-            to_write = '['
-            for w in state_1:
-                to_write += str(w) + ','
-            to_write = to_write[:-1]
-            to_write += ']'
-
-            bcfile.write(to_write)
-            bcfile.write(" ")
-
-            bcfile.write("[" + str(action) + "]")
-            bcfile.write(" ")
-
-            to_write = '['
-            for w in state_2:
-                to_write += str(w) + ','
-            to_write = to_write[:-1]
-            to_write += ']'
-
-            bcfile.write(to_write)
-            bcfile.write("\n")
-
-            # write to Diff file
-            to_write = '['
-            for w in state_1:
-                to_write += str(w) + ','
-            to_write = to_write[:-1]
-            to_write += ']'
-            dfile.write(to_write)
-
-            dfile.write(" ")
-            
-            to_write = '['
-            for w in state_2:
-                to_write += str(w) + ','
-            to_write = to_write[:-1]
-            to_write += ']'
-            dfile.write(to_write)
-            
-            dfile.write(" ")
-            to_write = '['
-
-            for w in diff:
-                to_write += str(w) + ','
-            to_write = to_write[:-1]
-            to_write += ']'
-
-            dfile.write(to_write)
-
-            dfile.write("\n")
- 
-
-            episode_rew += rew
-
-            steps += 1
-
-        print(steps)
-        print("Episode reward", episode_rew)
-        total_reward += episode_rew
-        episodes += 1.
-
-    outfile.close()
-    bcfile.close()
-
-    s=np.array(s)
-    ns=np.array(ns)
-    a=np.array(a)
-    d=np.array(d)
+    for MODE in range(2):
+        RANDOM = DEFAULT
+        num_steps = 50000
+        if MODE == test:
+            RANDOM = BEST
+            num_steps = 1000
     
-    print("Average reward", total_reward / episodes)
+        FILE = "Data/cartpole.txt"
+        BC_FILE = "Data/cartpole_bc.txt"
+        D_FILE = "Data/cartpole_d.txt"
+        
+        if MODE == test:
+            FILE = "Data/Tcartpole.txt"
+            BC_FILE = "Data/Tcartpole_bc.txt"
+            D_FILE = "Data/Tcartpole_d.txt"
 
-    # save state, action, nstate
-    if MODE == test:
-        filename = 'Data/TState.npy'
-        pickle.dump(s, open(filename, 'wb'))
-        filename = 'Data/TAction.npy'
-        pickle.dump(a, open(filename, 'wb'))
-        filename = 'Data/TNState.npy'
-        pickle.dump(ns, open(filename, 'wb'))
-        filename = 'Data/TDiff.npy'
-        pickle.dump(d,open(filename,'wb'))
-    else: 
+        s=[]
+        a=[]
+        ns=[]
+        d=[]
+        steps = 0
+        outfile = open(FILE, 'w')
+        bcfile = open(BC_FILE, 'w')
+        dfile = open(D_FILE, 'w')
+        
+        total_reward = 0
+        episodes = 0
+
+        while steps < num_steps:
+            obs, done = env.reset(), False
+            episode_rew = 0
+            while not done:
+                #env.render()
+                #if steps % 200==0:
+                #    obs, done = env.reset(), False
+                state_1 = obs
+
+                if np.random.uniform(0,1) <= RANDOM:
+                    action = act(obs[None])[0]
+                    obs, rew, done, _ = env.step(action)
+                else:
+                    action = env.action_space.sample()
+                    obs, rew, done, _ = env.step(action)
+                    continue
+                
+                s.append(state_1)
+                a.append(action)
+                state_2 = obs
+                diff = state_2 - state_1
+
+                ns.append(state_2)
+                d.append(diff)
+
+                # write to AON file
+                to_write = '['
+                for w in state_1:
+                    to_write += str(w) + ','
+                to_write = to_write[:-1]
+                to_write += ']'
+
+                outfile.write(to_write)
+                outfile.write(" ")
+                to_write = '['
+
+                for w in state_2:
+                    to_write += str(w) + ','
+                to_write = to_write[:-1]
+                to_write += ']'
+
+                outfile.write(to_write)
+                outfile.write("\n")
+
+                # write to BC file
+                to_write = '['
+                for w in state_1:
+                    to_write += str(w) + ','
+                to_write = to_write[:-1]
+                to_write += ']'
+
+                bcfile.write(to_write)
+                bcfile.write(" ")
+
+                bcfile.write("[" + str(action) + "]")
+                bcfile.write(" ")
+
+                to_write = '['
+                for w in state_2:
+                    to_write += str(w) + ','
+                to_write = to_write[:-1]
+                to_write += ']'
+
+                bcfile.write(to_write)
+                bcfile.write("\n")
+
+                # write to Diff file
+                to_write = '['
+                for w in state_1:
+                    to_write += str(w) + ','
+                to_write = to_write[:-1]
+                to_write += ']'
+                dfile.write(to_write)
+
+                dfile.write(" ")
+                
+                to_write = '['
+                for w in state_2:
+                    to_write += str(w) + ','
+                to_write = to_write[:-1]
+                to_write += ']'
+                dfile.write(to_write)
+                
+                dfile.write(" ")
+                to_write = '['
+
+                for w in diff:
+                    to_write += str(w) + ','
+                to_write = to_write[:-1]
+                to_write += ']'
+
+                dfile.write(to_write)
+
+                dfile.write("\n")
+    
+
+                episode_rew += rew
+
+                steps += 1
+
+            print(steps)
+            print("Episode reward", episode_rew)
+            total_reward += episode_rew
+            episodes += 1.
+
+        outfile.close()
+        bcfile.close()
+
+        s=np.array(s)
+        ns=np.array(ns)
+        a=np.array(a)
+        d=np.array(d)
+        
+        print("Average reward", total_reward / episodes)
+
         # save state, action, nstate
-        filename = 'Data/State.npy'
-        pickle.dump(s, open(filename, 'wb'))
-        filename = 'Data/Action.npy'
-        pickle.dump(a, open(filename, 'wb'))
-        filename = 'Data/NState.npy'
-        pickle.dump(ns, open(filename, 'wb'))
-        filename = 'Data/Diff.npy'
-        pickle.dump(d,open(filename,'wb'))
+        if MODE == test:
+            filename = 'Data/TState.npy'
+            pickle.dump(s, open(filename, 'wb'))
+            filename = 'Data/TAction.npy'
+            pickle.dump(a, open(filename, 'wb'))
+            filename = 'Data/TNState.npy'
+            pickle.dump(ns, open(filename, 'wb'))
+            filename = 'Data/TDiff.npy'
+            pickle.dump(d,open(filename,'wb'))
+        else: 
+            # save state, action, nstate
+            filename = 'Data/State.npy'
+            pickle.dump(s, open(filename, 'wb'))
+            filename = 'Data/Action.npy'
+            pickle.dump(a, open(filename, 'wb'))
+            filename = 'Data/NState.npy'
+            pickle.dump(ns, open(filename, 'wb'))
+            filename = 'Data/Diff.npy'
+            pickle.dump(d,open(filename,'wb'))
 
 
 if __name__ == '__main__':
